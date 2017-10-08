b34da4913d00734ab489ac2cdec084b787966a4c
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-08 23:19:14 +0900

    左右移動とジャンプの実装

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -6,24 +6,54 @@ public class UnityChanController : MonoBehaviour
 {
     private Animator myAnimator;
 
-	private Rigidbody myRigidbody;
+    private Rigidbody myRigidbody;
 
-	private float forwordForce = 800.0f;
+    private float forwordForce = 800.0f;
+
+    private float turnForce = 500.0f;
+
+    private float upForce = 500.0f;
+
+    private float movableRange = 3.4f;
 
     // Use this for initialization
     void Start()
     {
-		this.myAnimator = GetComponent<Animator>();
+        this.myAnimator = GetComponent<Animator>();
 
-		this.myAnimator.SetFloat("Speed", 1);
+        this.myAnimator.SetFloat("Speed", 1);
 
-		this.myRigidbody = GetComponent<Rigidbody>();
+        this.myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-		Debug.Log(this.transform.forward);
-		this.myRigidbody.AddForce(this.transform.forward * this.forwordForce);
+        Debug.Log(this.transform.forward);
+        // 前進
+        this.myRigidbody.AddForce(this.transform.forward * this.forwordForce);
+
+        // 左右移動
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            this.myRigidbody.AddForce(this.turnForce, 0, 0);
+        }
+
+        // ジャンプ
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            this.myAnimator.SetBool("Jump", true);
+
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
     }
 }
