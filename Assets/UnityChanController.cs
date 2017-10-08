6633fd1c145b804a3182fef760504285f57da850
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-08 21:31:08 +0900

    前進スクリプトの実装

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+    private Animator myAnimator;
+
+	private Rigidbody myRigidbody;
+
+	private float forwordForce = 800.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+		this.myAnimator = GetComponent<Animator>();
+
+		this.myAnimator.SetFloat("Speed", 1);
+
+		this.myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Debug.Log(this.transform.forward);
+		this.myRigidbody.AddForce(this.transform.forward * this.forwordForce);
+    }
+}
