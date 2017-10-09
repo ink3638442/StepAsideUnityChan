a53d2d4aa9463ce3b1a6f9e5a0ec128b33bf6499
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-09 16:31:00 +0900

    アイテム削除の実装

---
+++ Assets/ItemRemover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemRemover : MonoBehaviour {
+
+	private GameObject unitychan;
+
+	private const int BACK_DISTANCE = 5;
+	
+	// Use this for initialization
+	void Start () {
+		this.unitychan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - BACK_DISTANCE);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "CarTag"         || 
+		    other.gameObject.tag == "TrafficConeTag" || 
+			other.gameObject.tag == "CoinTag")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
