873b9ff63676b3b1a7a188597d5dee9f1fedfa1c
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-17 16:35:13 +0900

    リファクタリング

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class ItemGenerator : MonoBehaviour
 {
-	// プレファブはインスペクタ上で配置
+    // プレファブはインスペクタ上で配置
     public GameObject carPrefab;
 
     public GameObject coinPrefab;
 
     public GameObject conePrefab;
+
 
     private int startPos = -160;
 
@@ -17,66 +18,65 @@ public class ItemGenerator : MonoBehaviour
 
     private float posRange = 3.4f;
 
-	private GameObject unitychan;
 
-	private const int FRONT_DISTANCE = 50;
+    private GameObject unitychan;
 
-	// 生成する位置
-	int generatePos;
+	// unityちゃんからジェネレータの距離
+    private int frontDistance = 50;
+
+    // アイテムを生成する位置
+    int itemGeneratePos;
+
 
     // Use this for initialization
     void Start()
     {
-		this.unitychan = GameObject.Find("unitychan");
+        this.unitychan = GameObject.Find("unitychan");
 
-		//生成する位置の初期値
-		this.generatePos = startPos;
+        //生成する位置の初期値
+        this.itemGeneratePos = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-		this.transform.position = new Vector3(0, 0, this.unitychan.transform.position.z + FRONT_DISTANCE);
+        // ジェネレータをunityちゃんより前方50m先に設置
+        transform.position = new Vector3(0, 0, this.unitychan.transform.position.z + frontDistance);
 
-		if (this.startPos < this.transform.position.z && this.transform.position.z < goalPos)
-		{
-			// 前回作った位置より15m以上進んだら生成
-			if (this.generatePos < this.transform.position.z - 15)
-			{
-				Debug.Log(generatePos);
+        // ジェネレータが生成位置に到達したら生成する
+        if ((this.itemGeneratePos <= transform.position.z) && (transform.position.z < goalPos))
+        {
+            int num = Random.Range(0, 10);
+            if (num <= 1)
+            {
+                for (float j = -1; j <= 1; j += 0.4f)
+                {
+                    GameObject cone = Instantiate(conePrefab) as GameObject;
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, transform.position.z);
+                }
+            }
+            else
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    int item = Random.Range(1, 11);
+                    int offsetZ = Random.Range(-5, 6);
 
-				int num = Random.Range(0, 10);
-				if (num <= 1)
-				{
-					for (float j = -1; j <=	1; j += 0.4f)
-					{
-						GameObject cone = Instantiate(conePrefab) as GameObject;
-						cone.transform.position = new Vector3(4 * j, cone.transform.position.y, this.transform.position.z);
-					}
-				}
-				else
-				{
-					for (int j = -1; j < 2; j++)
-					{
-						int item = Random.Range(1, 11);
-						int offsetZ = Random.Range(-5, 6);
-						
-						if (1 <= item && item <= 6)
-						{
-							GameObject coin = Instantiate(coinPrefab) as GameObject;
-							coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, this.transform.position.z + offsetZ);
-						}
-						else if (7 <= item && item <= 9)
-						{
-							GameObject car = Instantiate(carPrefab) as GameObject;
-							car.transform.position = new Vector3(posRange * j, car.transform.position.y, this.transform.position.z + offsetZ); 
-						}
-					}
-				}
-				
-				// 次回生成する位置を更新
-				this.generatePos += 15;
-			}
-		}
+                    if (1 <= item && item <= 6)
+                    {
+                        GameObject coin = Instantiate(coinPrefab) as GameObject;
+                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, transform.position.z + offsetZ);
+                    }
+                    else if (7 <= item && item <= 9)
+                    {
+                        GameObject car = Instantiate(carPrefab) as GameObject;
+                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, transform.position.z + offsetZ);
+                    }
+                }
+            }
+
+            // 次回、アイテムを生成する位置を更新
+            this.itemGeneratePos += 15;
+        }
     }
 }
